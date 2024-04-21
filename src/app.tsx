218cd2a4a7943dda07c5f2becf88ab7a43a24d17
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-21 02:19:19 +0100

    Add Material Symbols

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import './index.css'
+import "./index.css";
+import "react-material-symbols/rounded";
 
 const queryClient = new QueryClient();
 
