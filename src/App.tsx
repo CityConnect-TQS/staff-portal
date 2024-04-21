f130c99ce271b2db907809b2a46e1a724a5496d3
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-21 01:47:41 +0100

    Add TanStack Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import './index.css'
 
 const queryClient = new QueryClient();
 
-export default function App() {
-  return (
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <div>
-          <p>Hello World</p>
-        </div>
-      </QueryClientProvider>
-    </ThemeProvider>
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("app")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StrictMode>
   );
 }
