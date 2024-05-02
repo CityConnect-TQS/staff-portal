9a0f00c10e4a39f56b0540055f90bf524623e02b
Diana <dianarrmiranda@gmail.com>
2024-05-02 22:20:34 +0100

    CC-54 Refactor themeSwitcher component in themeSwitcher.tsx

--- src/components/themeSwitcher.tsx
+++ src/components/themeSwitcher.tsx
@@ -6,27 +6,39 @@ import { useEffect, useState } from "react";
 import {MoonIcon} from "./moonIcon";
 import {SunIcon} from "./sunIcon";
 import {Button} from "@nextui-org/react";
+import { MaterialSymbol } from "react-material-symbols";
 
 export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  const switchTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else if (theme === 'dark') {
+      setTheme('system');
+    } else {
+      setTheme('light');
+    }
+  };
+
+  if (!mounted) return null;
 
   return (
     <div>
-        <Button isIconOnly variant="ghost" className="border-none" onClick={() => theme === 'light' ? setTheme('dark') : setTheme('light')}>
-          {theme === 'light' ? (
-            <SunIcon />
-            ) : (
-            <MoonIcon/>
-            )}
-        </Button>
-
+      <Button isIconOnly variant="ghost" className="border-none" onClick={switchTheme}>
+        {theme === 'light'? (
+          <SunIcon />
+        ) : theme === 'dark'? (
+          <MoonIcon />
+        ) : (
+          <MaterialSymbol  icon="computer" size={16} /> 
+        )}
+      </Button>
     </div>
-  )
+  );
 }
