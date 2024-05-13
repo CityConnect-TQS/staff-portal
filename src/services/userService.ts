dd1635174560c46f5e5146f501d67adc8587593b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-13 01:56:39 +0100

    Handle unsufficient permissions

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,19 +1,29 @@
 import { User, UserCreate, UserLogin } from "@/types/user";
 import { BACKOFFICE_BASE_API_URL, PUBLIC_BASE_API_URL } from "./config";
 
-export const loginUser = async (user: UserLogin): Promise<User> =>
-  fetch(PUBLIC_BASE_API_URL + "user/login", {
+export const loginUser = async (user: UserLogin): Promise<User> => {
+  const res = await fetch(PUBLIC_BASE_API_URL + "user/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(user),
-  }).then((res) => {
-    if (res.status === 401) {
-      throw new Error("Invalid credentials");
-    }
-    return res.json() as Promise<User>;
   });
+
+  if (res.status === 401) {
+    throw new Error("Invalid credentials");
+  }
+
+  const userResponse = (await res.json()) as User;
+  if (
+    !userResponse.roles.includes("ADMIN") &&
+    !userResponse.roles.includes("STAFF")
+  ) {
+    throw new Error("Unsufficient permissions");
+  }
+
+  return userResponse;
+};
 
 export const updateUser = async (
   id: number,
