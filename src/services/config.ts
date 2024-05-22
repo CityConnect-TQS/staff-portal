02f7a9168e4d1cf4b38777a3ca229d0ae1770d28
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-22 17:22:05 +0100

    Update the API path

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,3 @@
 const VITE_HOST_URL = import.meta.env.VITE_HOST_URL as string;
-export const BACKOFFICE_BASE_API_URL = `http://api.${VITE_HOST_URL}/api/backoffice/`;
-export const PUBLIC_BASE_API_URL = `http://api.${VITE_HOST_URL}/api/public/`;
+export const BACKOFFICE_BASE_API_URL = `http://${VITE_HOST_URL}/api/backoffice/`;
+export const PUBLIC_BASE_API_URL = `http://${VITE_HOST_URL}/api/public/`;
